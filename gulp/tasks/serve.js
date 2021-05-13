38432cb79743edb4944a00382591875fd604530e
Max <maxtarasevich@gmail.com>
2021-05-13 16:13:03 +0300

    project init

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp')
+
+const imageMinify = require('./imageMinify')
+const styles = require('./styles')
+const pug2html = require('./pug2html')
+const script = require('./script')
+
+const server = require('browser-sync').create()
+
+function readyReload(cb) {
+  server.reload()
+  cb()
+}
+
+module.exports = function serve(cb) {
+    server.init({
+        server: 'build',
+        notify: false,
+        open: true,
+        cors: true
+    })
+
+    gulp.watch('src/img/*.{gif,png,jpg,svg,webp}', gulp.series(imageMinify, readyReload))
+    gulp.watch('src/img/sprite/*.svg', gulp.series(readyReload))
+    gulp.watch('src/styles/**/*.scss', gulp.series(styles, cb => gulp.src('build/css').pipe(server.stream()).on('end', cb)))
+    gulp.watch('src/pages/**/*.scss', gulp.series(styles, cb => gulp.src('build/css').pipe(server.stream()).on('end', cb)))
+    gulp.watch('src/js/**/*.js', gulp.series(script, readyReload))
+    gulp.watch('src/pages/**/*.pug', gulp.series(pug2html, readyReload))
+   
+
+    gulp.watch('package.json', gulp.series(readyReload))
+
+    return cb()
+}
