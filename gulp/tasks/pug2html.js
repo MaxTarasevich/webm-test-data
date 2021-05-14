41ef4f53a0823a5d6b287e61365f0b14a0bb3f55
Max <maxtarasevich@gmail.com>
2021-05-15 00:20:26 +0300

    add JSON data to PUG

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -3,10 +3,14 @@ const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
 const config = require('../config')
 
+let data = require('../../src/data/data.json') // export data.json for pug
+
+
 module.exports = function pug2html() {
   return gulp.src('src/pages/*.pug')
     .pipe(plumber())
-    .pipe(pug({ pretty: config.pug2html.beautifyHtml }))
+    .pipe(pug({ pretty: config.pug2html.beautifyHtml,
+              locals:data}))
     .pipe(gulp.dest('build'))
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 console.log(`hello word`)
 let data = require('../data/data.json')
+let data2
 console.log(data)
 
 export default data;
